58aafb20d06dc4d17f1008324e2c1ccad722524a
Ryan Leach <rleach@rleach.id.au>
2014-04-22 20:32:24 +0930

    Configs for healthbars and hologram damage.

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ cls
 set /a c=0
 
 setlocal ENABLEDELAYEDEXPANSION
-
+:start
  for %%f in (*.jar) do (
 	set /a c=c+1
             echo !c!. %%~nf
@@ -15,7 +15,7 @@ setlocal ENABLEDELAYEDEXPANSION
  for %%f in (*.jar) do (
 	set /a c=c+1
             
-			:start
+			
 			if %chose%==!c!  java -Xdebug -Xrunjdwp:transport=dt_socket,address=1000,server=y,suspend=n -Xms1024M -Xmx1024M -XX:MaxPermSize=128M -jar %%f -o true
 			goto start
     )
